ed4232110d18611a893c201a1b221087c769ed72
xiao_mao <laurenli045@gmail.com>
2022-12-29 18:58:01 -0500

    moving all the zeros to the end while keeping order

---
+++ TwoPointers/src/MoveZeroes.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i<nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String args[]){
+        /*
+        Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+        Note that you must do this in-place without making a copy of the array.
+
+        Example 1:
+        Input: nums = [0,1,0,3,12]
+        Output: [1,3,12,0,0]
+
+        Example 2:
+        Input: nums = [0]
+        Output: [0]
+         */
+
+        int[] test1 = {0,1,0,3,12};
+        int[] test2 = {0};
+        int[] test3 = {0, 3, 5, 1, 1, 0, 7, 0};
+
+        moveZeroes(test1);
+        moveZeroes(test2);
+        moveZeroes(test3);
+    }
+}
